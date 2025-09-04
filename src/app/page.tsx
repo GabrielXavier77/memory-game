779d71684d3ad560e992de1877c75af456bfdc28
GabrielXavier77 <gabrielxavier270723@gmail.com>
2025-09-04 19:54:08 -0300

    memory-game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,303 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState, useEffect } from "react";
+import CardGame from "@/components/CardGame";
+
+type CardsProps = {
+  imageFlipped: string;
+  image: string;
+  flipped: boolean;
+  matched: boolean;
+};
+
+export default function MemoryGame() {
+  const [cardsList, setCardsList] = useState<CardsProps[]>([
+    {
+      imageFlipped:
+        "https://efootballhub.net/images/efootball24/players/106740406415422_l.webp",
+      image:
+        "https://opengameart.org/sites/default/files/card%20back%20blue.png",
+      flipped: false,
+      matched: false,
+    },
+    {
+      imageFlipped:
+        "https://efootballhub.net/images/efootball24/players/106740406415422_l.webp",
+      image:
+        "https://opengameart.org/sites/default/files/card%20back%20blue.png",
+      flipped: false,
+      matched: false,
+    },
+    {
+      imageFlipped:
+        "https://efootballhub.net/images/efootball24/players/88032602496343_l.webp",
+      image:
+        "https://opengameart.org/sites/default/files/card%20back%20blue.png",
+      flipped: false,
+      matched: false,
+    },
+    {
+      imageFlipped:
+        "https://efootballhub.net/images/efootball24/players/88032602496343_l.webp",
+      image:
+        "https://opengameart.org/sites/default/files/card%20back%20blue.png",
+      flipped: false,
+      matched: false,
+    },
+    {
+      imageFlipped:
+        "https://efootballhub.net/images/efootball24/players/88033139494397_l.webp",
+      image:
+        "https://opengameart.org/sites/default/files/card%20back%20blue.png",
+      flipped: false,
+      matched: false,
+    },
+    {
+      imageFlipped:
+        "https://efootballhub.net/images/efootball24/players/88033139494397_l.webp",
+      image:
+        "https://opengameart.org/sites/default/files/card%20back%20blue.png",
+      flipped: false,
+      matched: false,
+    },
+    {
+      imageFlipped:
+        "https://efootballhub.net/images/efootball24/players/105642505382303_l.webp",
+      image:
+        "https://opengameart.org/sites/default/files/card%20back%20blue.png",
+      flipped: false,
+      matched: false,
+    },
+    {
+      imageFlipped:
+        "https://efootballhub.net/images/efootball24/players/105642505382303_l.webp",
+      image:
+        "https://opengameart.org/sites/default/files/card%20back%20blue.png",
+      flipped: false,
+      matched: false,
+    },
+    {
+      imageFlipped:
+        "https://efootballhub.net/images/efootball24/players/52780584775806_l.webp",
+      image:
+        "https://opengameart.org/sites/default/files/card%20back%20blue.png",
+      flipped: false,
+      matched: false,
+    },
+    {
+      imageFlipped:
+        "https://efootballhub.net/images/efootball24/players/52780584775806_l.webp",
+      image:
+        "https://opengameart.org/sites/default/files/card%20back%20blue.png",
+      flipped: false,
+      matched: false,
+    },
+    {
+      imageFlipped:
+        "https://efootballhub.net/images/efootball24/players/88033139494377_l.webp",
+      image:
+        "https://opengameart.org/sites/default/files/card%20back%20blue.png",
+      flipped: false,
+      matched: false,
+    },
+    {
+      imageFlipped:
+        "https://efootballhub.net/images/efootball24/players/88033139494377_l.webp",
+      image:
+        "https://opengameart.org/sites/default/files/card%20back%20blue.png",
+      flipped: false,
+      matched: false,
+    },
+    {
+      imageFlipped:
+        "https://efootballhub.net/images/efootball24/players/105559290393784_l.webp",
+      image:
+        "https://opengameart.org/sites/default/files/card%20back%20blue.png",
+      flipped: false,
+      matched: false,
+    },
+    {
+      imageFlipped:
+        "https://efootballhub.net/images/efootball24/players/105559290393784_l.webp",
+      image:
+        "https://opengameart.org/sites/default/files/card%20back%20blue.png",
+      flipped: false,
+      matched: false,
+    },
+    {
+      imageFlipped:
+        "https://efootballhub.net/images/efootball24/players/88035555413453_l.webp",
+      image:
+        "https://opengameart.org/sites/default/files/card%20back%20blue.png",
+      flipped: false,
+      matched: false,
+    },
+    {
+      imageFlipped:
+        "https://efootballhub.net/images/efootball24/players/88035555413453_l.webp",
+      image:
+        "https://opengameart.org/sites/default/files/card%20back%20blue.png",
+      flipped: false,
+      matched: false,
+    },
+  ]);
+  const [selectedCards, setSelectedCards] = useState<number[]>([]);
+  const [isStarted, setIsStarted] = useState(false);
+  const [score, setScore] = useState(0);
+  const [time, setTime] = useState(0);
+  const [isWinner, setIsWinner] = useState(false);
+
+  useEffect(() => {
+    if (isStarted) {
+      if (isWinner) return;
+      setTimeout(() => {
+        setTime(time + 1);
+      }, 1000);
+    } else {
+      setTime(0);
+    }
+  }, [isStarted, isWinner, time]);
+
+  useEffect(() => {
+    if (score === 8) {
+      setIsWinner(true);
+    } else {
+      return;
+    }
+  }, [score]);
+
+  const handleStart = () => {
+    setCardsList(shuffleCards(cardsList))
+    setIsWinner(false);
+    setIsStarted(true);
+  };
+
+  const handleRestart = () => {
+    setIsStarted(false);
+    setIsWinner(false);
+    setScore(0);
+    setTime(0);
+    setSelectedCards([]);
+    const newCardsList = [...cardsList];
+    newCardsList.map((card) => {
+      card.flipped = false;
+      card.matched = false;
+    });
+  };
+
+  const handleTime = (timeInSeconds: number) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+
+    const zeroToLeft = (time: number) => time.toString().padStart(2, "0");
+
+    return `${zeroToLeft(minutes)}:${zeroToLeft(seconds)}`;
+  };
+
+  const shuffleCards = (cardsList: CardsProps[]): CardsProps[] => {
+    const newCardsList = [...cardsList];
+    for (let i = newCardsList.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newCardsList[i], newCardsList[j]] = [newCardsList[j], newCardsList[i]];
+    }
+    return newCardsList;
+  };
+
+  const toggleFlipped = (id: number) => {
+    if (!isStarted) return;
+
+    const newCardsList = [...cardsList];
+    const card = newCardsList[id];
+
+    if (card.flipped || card.matched || selectedCards.length === 2) return;
+
+    card.flipped = !card.flipped;
+
+    const newSelectedCardsList = [...selectedCards, id];
+    setSelectedCards(newSelectedCardsList);
+    setCardsList(newCardsList);
+
+    if (newSelectedCardsList.length === 2) {
+      const firstCard = newCardsList[newSelectedCardsList[0]];
+      const secondCard = newCardsList[newSelectedCardsList[1]];
+      if (firstCard.imageFlipped === secondCard.imageFlipped) {
+        firstCard.matched = true;
+        secondCard.matched = true;
+        setScore(score + 1);
+        setCardsList([...newCardsList]);
+        setSelectedCards([]);
+      } else {
+        setTimeout(() => {
+          newCardsList[newSelectedCardsList[0]].flipped = false;
+          newCardsList[newSelectedCardsList[1]].flipped = false;
+          setCardsList([...newCardsList]);
+          setSelectedCards([]);
+        }, 1000);
+      }
+    }
+  };
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="flex flex-col w-full justify-center items-center p-2">
+      {isStarted && !isWinner && (
+        <div className="grid grid-cols-4 w-[35%] gap-y-2 border-x-4 border-t-4 rounded-t-2xl border-purple-600 bg-purple-200 p-4 place-items-center">
+          {cardsList.map((card, index) => (
+            <CardGame
+              key={index}
+              id={index}
+              imageFlipped={card.imageFlipped}
+              image={card.image}
+              flipped={card.flipped}
+              matched={card.matched}
+              toggleFlipped={toggleFlipped}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      )}
+      {!isStarted && !isWinner && (
+        <div className="flex flex-col justify-center items-center w-[35%] h-[620px] border-x-4 border-t-4 rounded-t-2xl border-purple-600 bg-purple-200 p-4">
+          <p className="text-purple-600 text-3xl font-bold mb-4">
+            Jogo da Memoria!
+          </p>
+          <p className="text-lg">
+            Clique{" "}
+            <span className="px-4 py-2 font-bold rounded-2xl border-2 border-purple-600 text-purple-600 bg-purple-200">
+              Start
+            </span>{" "}
+            para começar
+          </p>
+        </div>
+      )}
+      {isWinner && (
+        <div className="flex flex-col justify-center items-center w-[35%] h-[620px] border-x-4 border-t-4 rounded-t-2xl border-purple-600 bg-purple-200 p-4">
+          <p className="text-purple-600 text-3xl font-bold mb-4">
+            Parabéns você venceu!
+          </p>
+          <p className="text-lg">Você ganhou em {handleTime(time)}</p>
+        </div>
+      )}
+      <div className="flex bg-purple-600 p-4 justify-between items-center rounded-b-2xl text-white h-[88px] w-[35%]">
+        <div className="flex flex-col justify-center items-center w-1/4">
+          <p className="font-bold text-xl">Score</p>
+          <p className="text-lg font-semibold">{score}</p>
+        </div>
+        <div className="flex flex-col justify-center items-center w-1/4 font-bold">
+          <button
+            className={`${
+              isStarted
+                ? "bg-red-600 text-white"
+                : "bg-purple-200 text-purple-600"
+            } p-4 text-xl cursor-pointer rounded-2xl w-full`}
+            onClick={isStarted ? () => handleRestart() : () => handleStart()}
+          >
+            {isStarted ? "Restart" : "Start"}
+          </button>
+        </div>
+        <div className="flex flex-col justify-center items-center w-1/4">
+          <p className="font-bold text-xl">Timer</p>
+          <p className="text-lg font-semibold">{handleTime(time)}</p>
+        </div>
+      </div>
     </div>
   );
 }
